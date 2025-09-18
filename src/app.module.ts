a578290870f0001a13efcebcbaae890684879772
Valentin Schiaffino <valenschg@gmail.com>
2025-09-17 21:00:09 -0300

    refactor: db config from .env

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,22 +1,26 @@
 import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { UsersModule } from 'src/infraestructure/rest-api/users/users.module';
-import { ConfigModule } from '@nestjs/config';
+import { ConfigModule, ConfigService } from '@nestjs/config';
 import { User } from 'src/infraestructure/entities/users/user.entity';
 import { UserPasswordRecovery } from 'src/infraestructure/entities/users/password-recovery.entity';
 
 @Module({
   imports: [
     ConfigModule.forRoot(),
-    TypeOrmModule.forRoot({
-      type: 'postgres',
-      host: 'localhost',
-      port: 5432,
-      username: 'postgres',
-      password: 'postgres',
-      database: 'estaller',
-      entities: [User, UserPasswordRecovery],
-      synchronize: true,
+    TypeOrmModule.forRootAsync({
+      imports: [ConfigModule],
+      useFactory: (configService: ConfigService) => ({
+        type: 'postgres',
+        host: configService.get<string>('DB_HOST', 'localhost'),
+        port: parseInt(configService.get<string>('DB_PORT', '5432'), 10),
+        username: configService.get<string>('DB_USERNAME', 'postgres'),
+        password: configService.get<string>('DB_PASSWORD', 'postgres'),
+        database: configService.get<string>('DB_DATABASE', 'estaller'),
+        entities: [User, UserPasswordRecovery],
+        synchronize: true,
+      }),
+      inject: [ConfigService],
     }),
     UsersModule,
   ],
