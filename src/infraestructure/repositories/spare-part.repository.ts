333fbe09bc42eba066007f42fa839bb5d5473377
Valentin Schiaffino <valenschg@gmail.com>
2025-10-01 19:55:58 -0300

    Fix/delete spare part from service (#20)
    * fix: delete spare part from service
    
    * fix: add mock for service service test

--- src/infraestructure/repositories/spare-part.repository.ts
+++ src/infraestructure/repositories/spare-part.repository.ts
@@ -7,6 +7,9 @@ import { PaginatedQueryDto } from 'src/domain/dtos/paginated-query.dto';
 import { PaginatedResultDto } from 'src/domain/dtos/paginated-result.dto';
 import { DataSource, ILike, In, Repository } from 'typeorm';
 import { SparePart } from '../entities/spare-part/spare-part.entity';
+import { ServiceSparePartDto } from '../dtos/services/create-service.dto';
+import { Service } from '../entities/service/service.entity';
+import { ServiceSparePart } from '../entities/service/service-spare-part.entity';
 
 @Injectable()
 export class SparePartRepository
@@ -15,6 +18,35 @@ export class SparePartRepository
 {
   constructor(private dataSource: DataSource) {
     super(SparePart, dataSource.createEntityManager());
+  }
+
+  async assignSparePartsToService(
+    service: Service,
+    dtos: ServiceSparePartDto[],
+  ): Promise<void> {
+    if (service.spareParts && service.spareParts.length > 0) {
+      await this.removeOldSparePartsFromService(service, dtos);
+    }
+    await ServiceSparePart.save(
+      dtos.map((sp) => ({
+        service: { id: service.id },
+        sparePart: { id: sp.sparePartId },
+        quantity: sp.quantity,
+      })) as ServiceSparePart[],
+    );
+  }
+
+  private async removeOldSparePartsFromService(
+    service: Service,
+    dtos: ServiceSparePartDto[],
+  ): Promise<void> {
+    const actualIds = service.spareParts.map((sp) => sp.sparePartId);
+    const updatedIds = dtos.map((sp) => sp.sparePartId);
+    const toRemoveIds = actualIds.filter((id) => !updatedIds.includes(id));
+    const entitiesToRemove = await ServiceSparePart.find({
+      where: { sparePartId: In(toRemoveIds), serviceId: service.id },
+    });
+    await ServiceSparePart.remove(entitiesToRemove);
   }
 
   getByIds(ids: number[]): Promise<SparePart[]> {
